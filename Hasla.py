a61d36670b5c6692e27f141a9a09f65ff74e5669
KubaK <gdx161252@student.gdansk.merito.pl>
2025-03-07 14:57:26 +0100

    Dzilajacy kod podstawowy z zapisem do pliku .json z głównymi funkcjonanościami.

--- Hasla.py
+++ Hasla.py
@@ -1,8 +1,7 @@
 import bcrypt
 import json
 
-dane_konta = {}
-
+dane_konta ={}
 
 def hash_password(password):
     salt = bcrypt.gensalt()
@@ -10,9 +9,8 @@ def hash_password(password):
     dane_konta["Haslo"] = x
     return x
 
-
 def zapisywanie():
-    with open('dane.json', 'r') as file:  # otwieranie pliku
+    with open('dane.json', 'r') as file: #otwieranie pliku
         users = json.load(file)
         users = [user for user in users if user.get("Login") != dane_konta["Login"]]
         users.append(dane_konta)  # dodawnie nowego uzytkownika do pliku
@@ -20,11 +18,9 @@ def zapisywanie():
         json.dump(users, file)
     print(users)
 
-
 def otwieranie():
     with open("dane.json", "r", encoding="utf-8") as file:
         return json.load(file)  # Zwracaj listę użytkowników
-
 
 def sprawdzanie_hasla():
     otwieranie()  # Wczytaj aktualne dane
@@ -38,36 +34,98 @@ def sprawdzanie_hasla():
         print("Hasło niezgodne!")
         return False
 
+def sprawdzanie_loginu():
+    login_do_sprawdzenia = input("Podaj swój login: ")
+    with open('dane.json', 'r') as file:
+        users = json.load(file)
+        for user in users:
+            if user.get("Login") != login_do_sprawdzenia:
+                continue
+            else:
+                print("Login prawidłowy")
+                print(user)
+                users.remove(user)
+                with open('dane.json', 'w') as file:
+                    json.dump(users, file, indent=4)
+                return True
+
+def usuwanie_danych_z_pliku():
+    usuwanie_danych_z_pliku = input("Podaj swój login: ")
+    with open('dane.json', 'r') as file:
+        users = json.load(file)
+        for user in users:
+            if user.get("Login") != usuwanie_danych_z_pliku:
+                continue
+            else:
+                print("Login prawidłowy")
+                do_sprawdzenia = input("Podaj Haslo do swojego konta")
+                print(user)
+                zapisane_haslo = user.get("Haslo")
+                if bcrypt.checkpw(do_sprawdzenia.encode(), zapisane_haslo.encode()):
+                    users.remove(user)
+                    with open('dane.json', 'w') as file:
+                        json.dump(users, file, indent=4)
+                    return True
+                else:
+                    return False
 
 class Konto:
-    def __init__(self, login, haslo):
+    def __init__(self,login, haslo):
         self.login = login
         self.haslo = haslo
 
-
 def tworzenie_konta():
-    otwieranie()
     login = input("Login: ")
-    haslo = input("Haslo: ")
-    haslo2 = input("podaj haslo ponownie: ")
-    if haslo == haslo2:
-        dane_konta["Login"] = login
-        dane_konta["Haslo"] = haslo
-        uzytkownik = Konto(login, haslo)
-        print(uzytkownik.login, uzytkownik.haslo)
-        hash_password(uzytkownik.haslo)
-        zapisywanie()
-    else:
-        print("haslo nieprawidłowe, spróbuj jeszcze raz")
-        tworzenie_konta()
+    with open('dane.json', 'r') as file:
+        users = json.load(file)
+        if any(user.get("Login") == login for user in users):
+            print("Konto z takim Loginem już istnieje \nSPRÓBUJ UŻYĆ INNEGO LOGINU \n")
+            return tworzenie_konta()
+
+        else:
+            haslo = input("Haslo: ")
+            haslo2 = input("podaj haslo ponownie: ")
+            if haslo == haslo2:
+                dane_konta["Login"] = login
+                dane_konta["Haslo"] = haslo
+                uzytkownik = Konto(login, haslo)
+                print(uzytkownik.login, uzytkownik.haslo)
+                hash_password(uzytkownik.haslo)
+                zapisywanie()
+            else:
+                print("haslo nieprawidłowe, spróbuj jeszcze raz")
+                tworzenie_konta()
+
+def logowanie():
+    login = input("Login: ")
+    with open('dane.json', 'r') as file:
+        users = json.load(file)
+        user = next((u for u in users if u.get("Login") == login), None)
+
+        if not user:
+            print("Login nie istnieje!")
+            return  # Jeśli login nie został znaleziony, kończymy funkcję
+
+        do_sprawdzenia = input("Hasło: ")
+        zapisane_haslo = user.get("Haslo")
+
+        if bcrypt.checkpw(do_sprawdzenia.encode(), zapisane_haslo.encode()):
+            dane_konta["Login"] = login
+            dane_konta["Haslo"] = zapisane_haslo
+            zapisywanie()
+            print("Poprawne dane ")
+            print("ZALOGOWANO ")
+        else:
+            print("Niepoprawne hasło!")
+            exit()  # Kończymy program po złym haśle
 
 
 def menu():
     global x
     while True:
         print("\n1: Zmiana Hasla")
-        print("2: Zmiana Loginu")
-        print("3: Sprawdzanie Hasla ")
+        print("2: Sprawdzanie Hasla")
+        print("3: Zmiana Loginu ")
         print("4: USUNIĘCIE KONTA ")
         print("5: Wyjście \n")
         wybor2 = int(input("Podaj co chcesz zrobic "))
@@ -84,18 +142,28 @@ def menu():
                     print("Podałeś niepoprawne hasło")
 
             case 2:
-                print("Tutaj bedzie zmiana loginu")
-
-            case 3:
                 sprawdzanie_hasla()
                 print('Haslo zapisane \n')
 
-            case 4:
-                print(' ========================== USUWANIE KONTA ==========================  \n')
+            case 3:
+                print("Tutaj bedzie zmiana loginu")
+                if sprawdzanie_loginu():
+                    nowy_login_do_zmiany = input("Podaj nowe login: ")
+                    dane_konta["Login"] = nowy_login_do_zmiany
+                    zapisywanie()
+                else:
+                    print("Login nieprawidlowy")
 
+            case 4:
+                print('========================== USUWANIE KONTA ==========================\n')
+                if usuwanie_danych_z_pliku():
+                    print("Konto zostało poprawnie usunięte")
+                    exit()
+                else:
+                    print("\n Podałeś nieprawidlowe dane ! ")
+                    menu()
             case 5:
                 exit()
-
 
 print("=============== Witaj w menedżerze hasel ===============")
 print('1. Aby stworzyc konto ')
@@ -109,13 +177,12 @@ while True:
         menu()
     elif wybor1 == '2':
         print("Tuaj bedzie logowanie")
+        logowanie()
         menu()
     elif wybor1 == '3':
         break
     else:
         print("NIPRAWIDLOWY WYBOR ! \nSPRÓBUJ JESZCZE RAZ")
 
-# Dodanie aby mozna bylo dodać dużo użytkowników
-# Dodanie funkcji logowania
-# Sprawdzenie czy taki uzytkownik o takim logniei istnieje jezeli tak to wymus zrobienie innego loginu
-# Dodainie dodatkowego sposobu uwierzytelniania
+#Dodainie dodatkowego sposobu uwierzytelniania
+#Zastąpienie powtarzająego sie kodu funkcjami.
